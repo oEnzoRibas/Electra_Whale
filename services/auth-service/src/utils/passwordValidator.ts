966e0b3f86ca3e91b182e21dd0e9e1023c0a851d
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2025-11-18 20:04:35 -0300

    Add password validation utility
    Introduces a password validation function and custom error class to enforce minimum length, uppercase, and numeric character requirements.

---
+++ services/auth-service/src/utils/passwordValidator.ts
@@ -0,0 +1,27 @@
+export class PasswordValidationError extends Error{
+    constructor(message: string) {
+        super(message);
+        this.name = "PasswordValidationError";
+    }
+}
+
+/**
+ * Validates Password
+ * @param password the password to be validated
+ */
+
+export function validatePassword(password: string): boolean{
+    if (password.length < 8){
+        throw new PasswordValidationError('Password must be at least 8 characters.')
+    }
+
+    if (!/[A-Z]/.test(password)) {
+        throw new PasswordValidationError('Password must contain at least one uppercase letter.');
+    }
+
+    if (!/[0-9]/.test(password)) {
+        throw new PasswordValidationError('Password must contain at least one number.')
+    }
+
+    return true;
+}
